92fda471186e56141738d9992fa7297763d727b9
Hubert Thieriot <hubert@energyandcleanair.org>
2022-03-09 21:29:53 +0800

    -adding update_counter function

---
+++ R/update.R
@@ -0,0 +1,22 @@
+update_counter <- function(){
+
+  library(tidyverse)
+  library(lubridate)
+
+
+  # update_flows(source="eurostat_exeu") # No need, always the same
+  update_flows(source="entsog")
+
+  prices <- price.get_modelled_price(flows_entsog=entsog.get_flows(use_cache=T),
+                                     flows_eurostat_exeu=eurostat_exeu.get_flows(use_cache=T))
+  db.upload_flows(flows=prices, source="combined")
+
+  # For faster loading
+  prices_light <- prices %>%
+    filter(date>="2022-01-01") %>%
+    group_by(date, source, commodity, transport, unit) %>%
+    summarise_at(c("value","value_eur"), sum, na.rm=T) %>%
+    filter(!is.na(source))
+
+  db.upload_flows(flows=prices_light, source="combined_light")
+}
