98a7b84ad73148f4f6b455cd07aa833e8d1b86e5
Hubert Thieriot <hubert@energyandcleanair.org>
2022-07-21 09:36:24 +0200

    -using EU and India separately

--- inst/shiny/server/section_payments.R
+++ inst/shiny/server/section_payments.R
@@ -1,6 +1,6 @@
 
 payments <- reactive({
-  url <- sprintf("%s/v0/counter?date_from=2022-01-01&format=csv&aggregate_by=date,destination_region,commodity_group&rolling_days=7", base_url)
+  url <- sprintf("%s/v0/counter?date_from=2022-01-01&format=csv&aggregate_by=date,destination_region,commodity_group&rolling_days=7&use_eu=True", base_url)
   payments <- utils.read_csv(url)
 
   return(payments)
@@ -12,14 +12,18 @@ output$plot_payments <- renderPlotly({
   p <- payments()
   req(p)
 
-  levels <- c("EU28", "China", "Turkey", "South Korea", "United States", "Others")
-  p <- p %>% mutate(destination_region=ifelse(destination_region %in% levels,
-                                         destination_region,
-                                         "Others")) %>%
-    mutate(destination_region = factor(destination_region, levels=levels)) %>%
-    group_by(destination_region, commodity_group, date) %>%
-    summarise_at(c("value_tonne", "value_eur"), sum, na.rm=T)
 
+  levels <- p %>%
+    group_by(destination_region) %>%
+    summarise(value_eur=sum(value_eur, na.rm=T)) %>%
+    arrange(desc(value_eur)) %>%
+    pull(destination_region)
+
+  p <- p %>%
+     mutate(destination_region = factor(destination_region, levels=levels))
+
+  #   group_by(destination_region, commodity_group, date) %>%
+  #   summarise_at(c("value_tonne", "value_eur"), sum, na.rm=T)
 
   colourCount = length(unique(p$commodity_group))
   getPalette = colorRampPalette(brewer.pal(12, "Paired"))
@@ -30,7 +34,7 @@ output$plot_payments <- renderPlotly({
     facet_wrap(~destination_region, nrow=3) +
     rcrea::theme_crea() +
     scale_y_continuous(limits=c(0, NA), expand=expansion(mult=c(0, 0.1))) +
-    scale_x_date(date_labels = "%b %Y", limits=c(as.Date("2022-01-15"), max(p$date) - lubridate::days(5))) +
+    scale_x_date(date_labels = "%b %y", limits=c(as.Date("2022-01-15"), max(p$date) - lubridate::days(5))) +
     scale_color_manual(values = getPalette(colourCount), name=NULL) +
     labs(x=NULL,
          y=NULL)
--- inst/shiny/ui/section_payments.R
+++ inst/shiny/ui/section_payments.R
@@ -1,5 +1,5 @@
 div(class="container container-payments",
   # h1("Daily payments to Russia"),
   div(class="subtitle", "Pipeline and shipments in million EUR per day"),
-  plotlyOutput("plot_payments", height='600px') %>% withSpinner(color="#8cc9D0")
+  plotlyOutput("plot_payments", height='700px') %>% withSpinner(color="#8cc9D0")
 )
