51867a4ae223c39b706416249e2e4a27e9eb547b
Hubert Thieriot <hubert.thieriot@gmail.com>
2023-06-27 15:34:29 +0000

    -date cast for entsog

--- R/source_entsog_new.R
+++ R/source_entsog_new.R
@@ -9,8 +9,8 @@ entsog_new.get_flows <- function(date_from='2021-11-01', date_to=NULL, use_cache
   years <- seq(lubridate::year(date_from), lubridate::year(date_to))
 
   flows <- lapply(years, function(year){
-    date_from_ <- max(date_from, paste0(year, '-01-01'))
-    date_to_ <- min(date_to, paste0(year, '-12-31'))
+    date_from_ <- max(as.Date(date_from), as.Date(paste0(year, '-01-01')))
+    date_to_ <- min(as.Date(date_to), as.Date(paste0(year, '-12-31')))
     read_csv(sprintf("https://api.russiafossiltracker.com/v0/entsogflow?type=crossborder,production&format=csv&date_from=%s&date_to=%s",
                      date_from_, date_to_))
   }) %>%
