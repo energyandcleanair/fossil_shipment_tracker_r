c7b5eabe8113da9a0f92a878bb2a75d2b5a6bf50
Panda Rushwood <panda@energyandcleanair.org>
2023-12-06 09:40:16 +0000

    Add option to rebuild pricing

--- R/update_counter.R
+++ R/update_counter.R
@@ -1,6 +1,4 @@
-
-update_counter <- function(){
-
+update_counter <- function(rebuild_prices = F) {
   library(tidyverse)
   library(lubridate)
   library(magrittr)
@@ -14,27 +12,27 @@ update_counter <- function(){
   # Price should come first, as many endpoints rely on this to be complete
   # Run the line below after updating price models
   # price.update_prices(production=T, rebuild = T)
-  price.update_prices(production=T, buffer_days=60)
+  price.update_prices(production = T, buffer_days = 60, rebuild = rebuild_prices)
 
   # Europe pipeline gas ------------------------------------------------------------
-  flows_entsog <- entsog_new.get_flows(date_from=lubridate::today()-21, use_cache=F)
+  flows_entsog <- entsog_new.get_flows(date_from = lubridate::today() - 21, use_cache = F)
   ok <- T
-  ok <- ok & (sum(flows_entsog$value_tonne) >= as.integer(max(flows_entsog$date)-min(flows_entsog$date)) * 5e5)
-  if(ok){
-    db.upload_flows_to_postgres(flows_entsog, production=T)
+  ok <- ok & (sum(flows_entsog$value_tonne) >= as.integer(max(flows_entsog$date) - min(flows_entsog$date)) * 5e5)
+  if (ok) {
+    db.upload_flows_to_postgres(flows_entsog, production = T)
   }
 
   # Other European overland flows ----------------------------------------------------
   flows_overland_eu <- overland_eu.get_flows()
-  db.upload_flows_to_postgres(flows_overland_eu, production=T)
+  db.upload_flows_to_postgres(flows_overland_eu, production = T)
 
   # China --------------------------------------------------------
   flows_china <- china.get_flows() %>%
-    mutate(value_mwh=NA_real_)
-  db.upload_flows_to_postgres(flows_china, production=T)
+    mutate(value_mwh = NA_real_)
+  db.upload_flows_to_postgres(flows_china, production = T)
 
   # Turkey --------------------------------------------------------
   flows_turkey <- turkey.get_flows() %>%
-    mutate(value_mwh=NA_real_)
-  db.upload_flows_to_postgres(flows_turkey, production=T)
+    mutate(value_mwh = NA_real_)
+  db.upload_flows_to_postgres(flows_turkey, production = T)
 }
