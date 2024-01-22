8c75d0f94eb053618c4a17dde49251c48d42d95e
Panda Rushwood <panda@energyandcleanair.org>
2024-01-22 15:12:46 +0000

    Improve code quality for wrapper

--- R/source_entsog_new.R
+++ R/source_entsog_new.R
@@ -3,42 +3,20 @@ entsog_new.get_flows <- function(date_from = "2021-11-01", date_to = NULL, use_c
     date_to <- as.character(lubridate::today() + lubridate::days(1))
   }
 
-  years <- seq(lubridate::year(date_from), lubridate::year(date_to))
+  flows <- entsog_new.get_flows_for_period(date_from, date_to) %>%
+    entsog_new.format_flows()
 
-  flows <- lapply(years, function(year) {
-    date_from_ <- max(as.Date(date_from), as.Date(paste0(year, "-01-01")))
-    date_to_ <- min(as.Date(date_to), as.Date(paste0(year, "-12-31")))
-    read_csv(sprintf(
-      "https://api.russiafossiltracker.com/v0/entsogflow?type=crossborder,production&format=csv&date_from=%s&date_to=%s",
-      date_from_, date_to_
-    ))
-  }) %>%
-    bind_rows()
+  flows_sourced <- process_iterative(flows)
 
-  flows <- flows %>% filter(destination_iso2 != "RU")
-
-  flows_formatted <- flows %>%
-    select(
-      from_country = commodity_origin_iso2,
-      to_country = commodity_destination_iso2,
-      date,
-      value = value_m3
-    )
-
-  flows_sourced <- process_iterative(flows_formatted)
-
-  sum(flows_sourced$value[flows_sourced$from_country == "RU"], na.rm = T) / 1e9
-  sum(flows_formatted$value[flows_formatted$from_country == "RU"]) / 1e9
-  sum(flows_sourced$value[flows_sourced$from_country == flows_sourced$to_country], na.rm = T) / 1e9 == 0
+  message(entsog_new.print_stats(flows, flows_sourced))
 
   # Add production
   flows_sourced <- bind_rows(
     flows_sourced %>%
       filter(from_country != to_country),
-    flows_formatted %>%
+    flows %>%
       filter(from_country == to_country)
   )
-
 
   flows_sourced <- flows_sourced %>%
     mutate(value = round(value, 2)) %>% # Some values are ~-1e-15
@@ -52,4 +30,47 @@ entsog_new.get_flows <- function(date_from = "2021-11-01", date_to = NULL, use_c
     )
 
   return(flows_sourced)
+}
+
+entsog_new.get_flows_for_period <- function(date_from, date_to) {
+  years <- seq(lubridate::year(date_from), lubridate::year(date_to))
+
+  flows <- lapply(years, function(year) {
+    entsog_new.get_flows_for_year(year, date_from, date_to)
+  }) %>%
+    bind_rows()
+
+  return(
+    flows %>%
+      filter(destination_iso2 != "RU")
+  )
+}
+
+entsog_new.get_flows_for_year <- function(year, date_from, date_to) {
+  date_from_ <- max(as.Date(date_from), as.Date(paste0(year, "-01-01")))
+  date_to_ <- min(as.Date(date_to), as.Date(paste0(year, "-12-31")))
+  return(
+    read_csv(sprintf(
+      "https://api.russiafossiltracker.com/v0/entsogflow?type=crossborder,production&format=csv&date_from=%s&date_to=%s",
+      date_from_, date_to_
+    ))
+  )
+}
+
+entsog_new.format_flows <- function(flows) {
+  return(
+    flows %>%
+      select(
+        from_country = commodity_origin_iso2,
+        to_country = commodity_destination_iso2,
+        date,
+        value = value_m3
+      )
+  )
+}
+
+entsog_new.print_stats <- function(flows, flows_sourced) {
+  sum(flows_sourced$value[flows_sourced$from_country == "RU"], na.rm = T) / 1e9
+  sum(flows$value[flows$from_country == "RU"]) / 1e9
+  sum(flows_sourced$value[flows_sourced$from_country == flows_sourced$to_country], na.rm = T) / 1e9 == 0
 }
